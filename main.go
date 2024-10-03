1079c35ef6ce7d37c80d1ec655dc9a3313b10c94
Max Mitchell <rat2043@gmail.com>
2024-10-02 20:17:23 -0700

    add img and comments

--- main.go
+++ main.go
@@ -18,6 +18,22 @@ import (
 	"golang.org/x/exp/rand"
 )
 
+/*
+ Hello this is my somewhat evangelion themed captcha (sorry I have not seen Stiens Gate)
+
+ The goal is to click on the hexes on the big hex grid such that the red hexes match the ones in the tiny hex grid
+
+ compile by running go build main.go
+ (you may need to install the dependencies listed here https://ebitengine.org/en/documents/install.html)
+ (also to cross compile to windows run `GOOS=windows GOARCH=amd64 ^C build main.go`)
+
+ then just run that exe
+
+ Thank you so much for checking out my captcha, had to throw this together before I left on a trip to Europe!
+
+ LOVE THE CHANNEL SO MUCH!!!!! <3
+*/
+
 //go:embed assets/imgs/*
 var assets embed.FS
 
